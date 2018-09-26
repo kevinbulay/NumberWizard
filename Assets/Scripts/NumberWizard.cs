3f5aba4f1593c59328ac9805523391ef0975825d
Kevin Bulay <kevinbulay@macbook-pro-2.edu.htl.org>
2018-09-26 14:22:39 +0200

    Spiel fertig

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+    int min;
+    int max;
+    int guess;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	void Start () 
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+        
+        Debug.Log("I$T DA$$ DEINE ZAHL " + guess + "?");
+    }
+        
 	// Update is called once per frame
 	void Update () {
-		
+          
+        //lower
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+        max = guess;
+            
+            Vermutung();
+        }
+        
+        //higher
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+        min = guess;
+            Vermutung();
+        }
+         else if (Input.GetKeyDown(KeyCode.Return))
+        {
+        Debug.Log("FOLGT @PANZERBANDE");
+        }
+
 	}
+    void Vermutung ()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("I$T DA$$ DEINE ZAHL " + guess + "?");
+        
+    }
 }
